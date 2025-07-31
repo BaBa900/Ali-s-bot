ad2adb98c282b08a3f045ccf693d98e02162fa53
BaBa900 <142046786+BaBa900@users.noreply.github.com>
2025-07-31 11:35:28 +0500

    Update admin_det.js

--- Priyansh/commands/admin_det.js
+++ Priyansh/commands/admin_det.js
@@ -3,7 +3,7 @@ module.exports.config = {
   name: "admin2",
   version: "1.0.1",
   hasPermssion: 0,
-  credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩", 
+  credits: "꧁𓊈𒆜🅰🅻🅸 🅺🅷🅰🅽𒆜𓊉꧂", 
   description: "hihihihi",
   commandCategory: "no prefix",
   usages: "admin",
