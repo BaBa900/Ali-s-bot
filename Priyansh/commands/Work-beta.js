14561a98f000a7bf7e43e4a6862a54fadf77a50c
BaBa900 <142046786+BaBa900@users.noreply.github.com>
2025-07-31 11:29:07 +0500

    Update Work-beta.js

--- Priyansh/commands/Work-beta.js
+++ Priyansh/commands/Work-beta.js
@@ -3,7 +3,7 @@ module.exports.config = {
     name: "job",
     version: "1.0.2",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭", 
+    credits: "꧁𓊈𒆜🅰🅻🅸 🅺🅷🅰🅽𒆜𓊉꧂", 
     description: "",
     commandCategory: "Economy",
     cooldowns: 5,
