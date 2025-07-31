822e72018f64f43d703e721a4c275a7822b09292
BaBa900 <142046786+BaBa900@users.noreply.github.com>
2025-07-31 11:27:43 +0500

    Update Rushi.js

--- Priyansh/commands/Rushi.js
+++ Priyansh/commands/Rushi.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "rushia",
  version: "1.0.0",
  hasPermssion: 0,
- credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+ credits: "꧁𓊈𒆜🅰🅻🅸 🅺🅷🅰🅽𒆜𓊉꧂",
  description: "Random Rushia",
  commandCategory: "random-img",
  usages: "rushia",
