3bd96c471784939529f8de9b392310ae90efa81e
BaBa900 <142046786+BaBa900@users.noreply.github.com>
2025-07-31 11:25:09 +0500

    Update Google.js

--- Priyansh/commands/Google.js
+++ Priyansh/commands/Google.js
@@ -5,7 +5,7 @@ module.exports.config = {
 
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "꧁𓊈𒆜🅰🅻🅸 🅺🅷🅰🅽𒆜𓊉꧂",
 	description: "Comment on table ( ͡° ͜ʖ ͡°)",
 	commandCategory: "edit-img",
 	usages: "google [text]",
