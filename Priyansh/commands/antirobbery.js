b9ab4a5a2450dec8a9f30bfb9f461f1ea59d1072
BaBa900 <142046786+BaBa900@users.noreply.github.com>
2025-07-31 11:42:14 +0500

    Update antirobbery.js

--- Priyansh/commands/antirobbery.js
+++ Priyansh/commands/antirobbery.js
@@ -1,7 +1,7 @@
 module.exports.config = {
  name: "antirobbery",
  version: "1.0.0",
- credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+ credits: "꧁𓊈𒆜🅰🅻🅸 🅺🅷🅰🅽𒆜𓊉꧂",
  hasPermssion: 1,
  description: "Prevent changing group administrators",
  usages: "",
