0aaadbf4a02cba2f86486ffceb9b6ce5392850d2
BaBa900 <142046786+BaBa900@users.noreply.github.com>
2025-07-31 11:27:23 +0500

    Update Rank.js

--- Priyansh/commands/Rank.js
+++ Priyansh/commands/Rank.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "rank",
 	version: "2.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "꧁𓊈𒆜🅰🅻🅸 🅺🅷🅰🅽𒆜𓊉꧂",
 	description: "View Member Rankings",
 	commandCategory: "Group",
 	usages: " [user] or [tag]",
