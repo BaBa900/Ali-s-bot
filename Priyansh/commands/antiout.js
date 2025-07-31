b11397eac3e60fb22a2e828c4b73c86f91f0b0d7
BaBa900 <142046786+BaBa900@users.noreply.github.com>
2025-07-31 11:41:47 +0500

    Update antiout.js

--- Priyansh/commands/antiout.js
+++ Priyansh/commands/antiout.js
@@ -1,7 +1,7 @@
 module.exports.config = {
     name: "antiout",
     version: "1.0.0",
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "꧁𓊈𒆜🅰🅻🅸 🅺🅷🅰🅽𒆜𓊉꧂",
     hasPermssion: 1,
     description: "Turn off antiout",
     usages: "antiout on/off",
