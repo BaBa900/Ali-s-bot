97cb1f0d672d03107d81ede585ae678d50509ef4
BaBa900 <142046786+BaBa900@users.noreply.github.com>
2025-07-31 11:16:59 +0500

    Update 0shayri.js

--- Priyansh/commands/0shayri.js
+++ Priyansh/commands/0shayri.js
@@ -3,7 +3,7 @@ module.exports.config = {
   name: "shayri",
   version: "1.0.1",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "꧁𓊈𒆜🅰🅻🅸 🅺🅷🅰🅽𒆜𓊉꧂",
   description: "Ranmdom a Shayri",
   commandCategory: "group",
   usages: "shayri",
